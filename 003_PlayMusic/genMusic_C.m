6c323f0edae50a9e551fa9aeb0f37182df56282d
Chunyu <chunyu2018@foxmail.com>
2020-01-13 09:38:42 +0800

    引入钢琴音色
    main：
        1. 引入钢琴谐波1~8（不包含7）；
        参考：https://zhuanlan.zhihu.com/p/27112291

--- 003_PlayMusic/genMusic_C.m
+++ 003_PlayMusic/genMusic_C.m
@@ -29,7 +29,7 @@ function msc = genMusic_C(varargin)
     
     for k = 1:N
         t = 0:1/fs:rythm(k,2)*Tb;
-        win = (1-t/t(end));
+        win = 1-t/t(end);
         
         switch rythm(k,1)
             case 0
@@ -38,26 +38,27 @@ function msc = genMusic_C(varargin)
             case {1 2 3 4 5 6 7}
                 fidx = rythm(k,1);
                 f = [261.6256  293.6648  329.6276  349.2282  391.9954  440.0000  493.8833];
-                msc0{k} = sin(f(fidx)*2*pi*t).*win;
+                msc0{k} = simPiano(f(fidx),t,win);
                 
             case {11 12 13 14 15 16 17}
                 fidx = rythm(k,1)-10;
                 f = [523.2500  587.3300  659.2600  698.4600  783.9900  880.0000  987.7700];
-                msc0{k} = sin(f(fidx)*2*pi*t).*win;
+                msc0{k} = simPiano(f(fidx),t,win);
                 
             case {21 22 23 24 25 26 27}
                 fidx = rythm(k,1)-20;
                 f = [1.0465    1.1747    1.3185    1.3969    1.5680    1.7600    1.9755]*1e3;
-                msc0{k} = sin(f(fidx)*2*pi*t).*win;
+                msc0{k} = simPiano(f(fidx),t,win);
                 
             case {-11 -12 -13 -14 -15 -16 -17}
                 fidx = abs(rythm(k,1)+10);
                 f = [130.8100  146.8300  164.8100  174.6100  196.0000  220.0000  246.9400];
-                msc0{k} = sin(f(fidx)*2*pi*t).*win;
+                msc0{k} = simPiano(f(fidx),t,win);
+                
             case {-21 -22 -23 -24 -25 -26 -27}
                 fidx = abs(rythm(k,1)+20);
                 f = [65.4060   73.4160   82.4070   87.3070   97.9990  110.0000  123.4700];
-                msc0{k} = sin(f(fidx)*2*pi*t).*win;
+                msc0{k} = simPiano(f(fidx),t,win);
                 
             otherwise
                 msc0{k} = [];
@@ -65,4 +66,14 @@ function msc = genMusic_C(varargin)
     end
     
     msc = reshape([msc0{:}],[],1);
+end
+
+function y = simPiano(f,t,win)
+    y = (sin(f*2*pi*t) + ...
+        0.1000*sin(f*4*pi*t) + ...
+        0.0631*sin(f*6*pi*t) + ...
+        0.0447*sin(f*8*pi*t) + ...
+        0.0398*sin(f*10*pi*t) + ...
+        0.0178*sin(f*12*pi*t) + ...
+        0.0178*sin(f*16*pi*t) ).*win;
 end
